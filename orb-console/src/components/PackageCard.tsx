40a248905987d55341a579b42c610f28a1147798
Christopher Ariza <ariza@flexatone.com>
2025-04-03 17:43:47 -0700

    upated packate card

--- orb-console/src/components/PackageCard.tsx
+++ orb-console/src/components/PackageCard.tsx
@@ -6,17 +6,32 @@ type PackageCardProps = {
 
 export function PackageCard({ pkg }: PackageCardProps) {
   return (
-    <div className="p-4 border border-slate-600 rounded-lg bg-gray-800 shadow-md text-sm w-full ">
-      <h3 className="font-bold ">{pkg.name}</h3>
-      <p className="text-gray-500">Key: {pkg.key}</p>
-      <p className="text-gray-400">Version: {pkg.version}</p>
-      {pkg.direct_url && (
-        <p className="text-blue-600">
-          <a href={pkg.direct_url} target="_blank" rel="noopener noreferrer">
-            Source
-          </a>
-        </p>
-      )}
+    <div className="p-2 pl-4 border border-slate-600 rounded-lg bg-gray-800 shadow-md text-sm w-full">
+      <h3 className="font-bold mb-1">{pkg.name}</h3>
+
+      <div className="flex flex-col gap-1 text-gray-400">
+        <div className="flex gap-2">
+          <span className="w-20 text-gray-500">Key:</span>
+          <span>{pkg.key}</span>
+        </div>
+        <div className="flex gap-2">
+          <span className="w-20 text-gray-500">Version:</span>
+          <span>{pkg.version}</span>
+        </div>
+        {pkg.direct_url && (
+          <div className="flex gap-2">
+            <span className="w-20 text-gray-500">Source:</span>
+            <a
+              href={pkg.direct_url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 underline"
+            >
+              Link
+            </a>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
