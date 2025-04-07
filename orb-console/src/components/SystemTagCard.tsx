c5d0c92a2a4eb86dfc07c91fbe26211535439539
Christopher Ariza <ariza@flexatone.com>
2025-04-06 21:13:11 -0700

    sorted results

--- orb-console/src/components/SystemTagCard.tsx
+++ orb-console/src/components/SystemTagCard.tsx
@@ -66,19 +66,25 @@ export function SystemTagCard({
             </tr>
           </thead>
           <tbody>
-            {tag.pings.map((ping) => (
-              <tr
-                key={ping.timestamp}
-                className={`border-b border-slate-800 ${
-                  ping.scanned ? "bg-gray-700" : "bg-gray-900"
-                }`}
-              >
-                <td className="px-2 py-1 whitespace-nowrap">
-                  {new Date(ping.timestamp).toLocaleTimeString()}
-                </td>
-                <td className="px-2 py-1">{ping.scanned ? "✓" : "–"}</td>
-              </tr>
-            ))}
+            {[...tag.pings]
+              .sort(
+                (a, b) =>
+                  new Date(b.timestamp).getTime() -
+                  new Date(a.timestamp).getTime(),
+              )
+              .map((ping) => (
+                <tr
+                  key={ping.timestamp}
+                  className={`border-b border-slate-800 ${
+                    ping.scanned ? "bg-gray-900" : "bg-gray-900"
+                  }`}
+                >
+                  <td className="px-2 py-1 whitespace-nowrap">
+                    {new Date(ping.timestamp).toLocaleTimeString()}
+                  </td>
+                  <td className="px-2 py-1">{ping.scanned ? "✓" : "–"}</td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
