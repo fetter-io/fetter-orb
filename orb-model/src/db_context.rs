7e4974c8884db89ef4b8cf7262e5adde6cea1e43
Christopher Ariza <ariza@flexatone.com>
2025-10-09 20:03:17 -0700

    updated

--- orb-model/src/db_context.rs
+++ orb-model/src/db_context.rs
@@ -1285,7 +1285,7 @@ impl DBContext {
         // NOTE: as we defer updating audit data unless (a) user explicitly asks for it or (b) shouldAuditUpdate is true (packages change / duration limit passed), we may not need to use cache_dur here, which will use file-based caching on the back-end
         let cache_dur = Duration::from_secs(0);
         let cache_dir = path_cache(true).expect("Could not create path");
-        let cache_config = CacheConfig::new(cache_dur, &cache_dir);
+        let cache_config = CacheConfig::new(cache_dur, cache_dir);
 
         let audit = AuditReport::from_packages(
             client,
