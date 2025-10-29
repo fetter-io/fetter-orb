9dcb5fc47ae879c84ce4edfedb066f6ffaeb88b0
flexatone <you@example.com>
2025-10-29 15:30:10 -0700

    updated system card presentation

--- orb-console/src/components/SystemTagCard.tsx
+++ orb-console/src/components/SystemTagCard.tsx
@@ -80,7 +80,7 @@ export function SystemTagCard({
         </div>
       </div>
 
-      <div className="grid grid-cols-2 gap-2 mb-2 break-all">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-2 mb-2 break-all text-sm text-gray-400">
         <div>
           <span className="text-gray-500">OS: </span>
           <span>
