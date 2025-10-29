a7c4067924cbe029ce1bdd1d97cb2cef94ce5f1d
flexatone <you@example.com>
2025-10-29 09:41:42 -0700

    updated icon to use SVG

--- orb-console/src/components/SystemTagActiveIcon.tsx
+++ orb-console/src/components/SystemTagActiveIcon.tsx
@@ -1,3 +1,5 @@
+import colors from "tailwindcss/colors";
+
 type SystemTagActiveIconProps = {
   active: boolean;
   isUpdating: boolean;
@@ -12,13 +14,34 @@ export function SystemTagActiveIcon({
   return (
     <button
       title={active ? "Deactivate system" : "Activate system"}
-      className={`w-4 h-4 bg-gray-900 flex-shrink-0 overflow-visible text-clip rounded-xs flex items-center justify-center ring-1 ring-gray-600 font-black hover:bg-gray-600 cursor-pointer transition-colors ${
-        active ? "text-green-600" : "text-red-400"
-      } ${isUpdating ? "opacity-50 cursor-not-allowed" : ""}`}
+      className={`w-4 h-4 bg-gray-900 flex-shrink-0 rounded-xs flex items-center justify-center ring-1 ring-gray-600 hover:bg-gray-600 cursor-pointer transition-colors ${
+        isUpdating ? "opacity-50 cursor-not-allowed" : ""
+      }`}
       onClick={onToggle}
       disabled={isUpdating}
     >
-      {active ? "●" : "○"}
+      {active ? (
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="6" cy="6" r="3" fill={colors.green[700]} />
+        </svg>
+      ) : (
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="6" cy="6" r="3" fill={colors.red[400]} />
+
+        </svg>
+      )}
     </button>
   );
 }
