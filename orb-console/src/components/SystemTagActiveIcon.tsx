32ba6476e726deffc8c4d78cb408d58447364d74
flexatone <you@example.com>
2025-10-29 09:42:16 -0700

    updated

--- orb-console/src/components/SystemTagActiveIcon.tsx
+++ orb-console/src/components/SystemTagActiveIcon.tsx
@@ -39,7 +39,6 @@ export function SystemTagActiveIcon({
           xmlns="http://www.w3.org/2000/svg"
         >
           <circle cx="6" cy="6" r="3" fill={colors.red[400]} />
-
         </svg>
       )}
     </button>
